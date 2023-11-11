c69a9ecdc166e4b37b16552d6f7ee8db876a6fbc
cosmic-helios <136916325+cosmic-helios@users.noreply.github.com>
2023-11-11 12:11:47 +0530

    Connection with Database Recipie Book

--- main/RecipieBook.py
+++ main/RecipieBook.py
@@ -1,26 +1,36 @@
 #Importing MYSQL Connector
 import mysql.connector as cnt
 
-#Establishing the connection with database
-mydb = cnt.connect(host = 'localhost',
+#Establishing the connection with MYSQL
+mysql = cnt.connect(host = 'localhost',
                                user = 'aurelius',
                                password = 'aurelius.141')
-#Checking the connection
-if mydb.is_connected():
-    print ("### Connection Established ###")
+
+#Checking for the desired database and creating it otherwise
+if mysql.is_connected():
+    print ("### Connection Established with MYSQL ###")
 else:
     print("### Connection Failed ###")
-
-#Creating a cursor object
-exe = mydb.cursor()
+exe = mysql.cursor()
 
 db = exe.execute("SHOW DATABASES")
 db_r = exe.fetchall()
 
-if ('Recipie_Book',) in db_r:
+def database():
+    print("Changing Database")
+    mydb = cnt.connect(host = 'localhost',
+                               user = 'aurelius',
+                               password = 'aurelius.141',
+                               database = 'Recipe_Book')
+    if mydb.is_connected():
+        print("Connection Established with Recipie_Book")
+    else:
+        print("Connection Failed")
+    
+if ('Recipe_Book',) in db_r:
     print('*Database Exists*')
+    database()
 else:
     print('*Database not found*')
     print('*Creating Database*')
-    db_c = exe.execute('CREATE DATABASE Recipie_Book')
-
+    db_c = exe.execute('CREATE DATABASE Recipe_Book')
