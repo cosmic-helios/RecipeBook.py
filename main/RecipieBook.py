3ecee0ce9db2ad5c942606c10ad9f753fed21d63
cosmic-helios <136916325+cosmic-helios@users.noreply.github.com>
2023-11-11 13:22:58 +0530

    Added Formatting to the terminal window

--- main/RecipieBook.py
+++ main/RecipieBook.py
@@ -8,7 +8,10 @@ mysql = cnt.connect(host = 'localhost',
 
 #Checking for the desired database and creating it otherwise
 if mysql.is_connected():
-    print ("### Connection Established with MYSQL ###")
+    print("---------------------------------------------------------------------------------")
+    print("|                 ### Connection Established with MYSQL ###                     |")
+    print("---------------------------------------------------------------------------------")
+
 else:
     print("### Connection Failed ###")
 exe = mysql.cursor()
@@ -18,15 +21,18 @@ db_r = exe.fetchall()
 
 def database():
     print("Changing Database")
+    global mydb
     mydb = cnt.connect(host = 'localhost',
                                user = 'aurelius',
                                password = 'aurelius.141',
                                database = 'Recipe_Book')
     if mydb.is_connected():
-        print("Connection Established with Recipie_Book")
+        print("---------------------------------------------------------------------------------")
+        print("|               ### Connection Established with Recipie_Book ###                |")
+        print("---------------------------------------------------------------------------------")
     else:
         print("Connection Failed")
-    
+   
 if ('Recipe_Book',) in db_r:
     print('*Database Exists*')
     database()
@@ -34,3 +40,24 @@ else:
     print('*Database not found*')
     print('*Creating Database*')
     db_c = exe.execute('CREATE DATABASE Recipe_Book')
+    database()
+
+cur = mydb.cursor()
+#CREATING TABLES
+tables = cur.execute("SHOW TABLES")
+tables_r = cur.fetchall()
+if ('RECIPES',) not in tables_r :
+    cur.execute("CREATE TABLE RECIPES(recipe_name varchar(255) primary key,cuisine varchar(255),country varchar(20))")
+
+def CHOICES():
+    print("---------------------------------------------------------------------------------")
+    print('|                          WHAT ARE YOU HERE FOR TODAY?                         |')
+    print('---------------------------------------------------------------------------------')
+    print("|                   Enter VIEW for viewing existing Recipies                    |")
+    print('|             Enter ADD if you want to save your personal recipes               |')
+    print('|         Enter TRY if you want instructions to try a certain recipe            |')
+    print('---------------------------------------------------------------------------------')
+    print()
+    choice = input("Enter your choice: ")
+
+CHOICES()
